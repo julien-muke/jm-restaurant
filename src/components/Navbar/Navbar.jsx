90ae46f898a46c5d6afe8dc63ad33127ff118cd5
julien-muke <julien.muke@gmail.com>
2022-12-16 07:05:27 +0200

    navbar smallscreen

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import images from '../../constants/images';
 import './Navbar.css';
 
-const Navbar = () => (
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+
+  return (
   <nav className='app__navbar'>
     <div className='app__navbar-logo'>
       <img src={images.gericht} alt="app logo" />
@@ -21,7 +24,24 @@ const Navbar = () => (
       <div />
       <a href='/' className='p__opensans'>Book Table</a>
     </div>
+    <div className='app__navbar-smallscreen'>
+        <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => setToggleMenu(true)} />
+
+        {toggleMenu && (
+        <div className='app__navbar-smallscreen_overlay flex__center silde-bottom'>
+          <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+          <ul className='app__navbar-smallscreen_links'>
+            <li className='p__opensans'><a href='#home'>Home</a></li>
+            <li className='p__opensans'><a href='#home'>About</a></li>
+            <li className='p__opensans'><a href='#home'>Menu</a></li>
+            <li className='p__opensans'><a href='#home'>Awards</a></li>
+            <li className='p__opensans'><a href='#home'>Contact</a></li>
+         </ul>
+        </div>
+        )}
+    </div>
   </nav>
-);
+  )
+  }
 
 export default Navbar;
